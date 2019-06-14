1625603b0729bc26175349e118620cef8e547fe4
tt <tamara.trefilova@gmail.com>
2019-06-14 10:05:08 -0700

    asdded aome functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
-var userNum;
-
-
+var inputNum;
+var arrayNum;
+var result = "";
 
 
 $(function() {
   $("#inputForm").submit(function(event) {
     event.preventDefault();
 
-
-    userNum = parseInt($("#userInput").val());
-    alert(userNum);
-
-
+    var inputString = $("#userInput").val();
+    inputNum = parseInt(inputString);
+    alert("input as a number: "+inputNum);
+    arrayNum = inputString.split();
+    alert(arrayNum);
+    var arrayLength = arrayNum.length;
+    if (arrayLength == 1) {
+      for (var i = 0; i <= inputNum; i++) {
+        result += i+" ,";
+      }
+    }
+    alert("Result: "+result);
   });
 });
